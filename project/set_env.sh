493efd2adf73c9904345160becaefbcfbc4f2712
osman8218 <osmannm18@gmail.com>
2021-09-02 17:56:04 -0500

    new repo for project

---
+++ project/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresdb
+export POSTGRES_PASSWORD=Toyota8218!
+export POSTGRES_HOST=postgres.cvtubr8s5yok.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=osman-udagram-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
